25e41a0c1fb85ec3e1e8b9fdc7ef76c17e27bd0e
lakshmimungara <lakshmimungara2997@gmail.com>
2024-10-04 08:06:37 +0530

    added openvpn concept

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -67,6 +67,15 @@ module "app_alb_sg" {
   common_tags  = var.common_tags
   sg_tags      = var.app_alb_sg_tags
 }
+
+module "vpn_sg" {
+  source       = "git::https://github.com/lakshmimungara/terraform-aws-security-group.git?ref=main"
+  project_name = var.project_name
+  environment  = var.environment
+  sg_name      = "vpn"  # expense-dev-app-alb
+  vpc_id       = local.vpc_id
+  common_tags  = var.common_tags
+}
 # Mysql allowing connections on 3306 from the instances attached to backend security group 
 # mysql --> backend
 resource "aws_security_group_rule" "mysql_backend" {
@@ -187,4 +196,68 @@ resource "aws_security_group_rule" "app_alb_bastion" {
   protocol          = "tcp"
   source_security_group_id = module.bastion_sg.id 
   security_group_id = module.app_alb_sg.id # to which security_group we are assigning the rule
+}
+
+resource "aws_security_group_rule" "vpn_public" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.id # to which security_group we are assigning the rule
+}
+
+
+resource "aws_security_group_rule" "vpn_public_443" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.id # to which security_group we are assigning the rule
+}
+
+resource "aws_security_group_rule" "vpn_public_943" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.id # to which security_group we are assigning the rule
+}
+
+resource "aws_security_group_rule" "vpn_public_1194" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.id # to which security_group we are assigning the rule
+}
+
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.id 
+  security_group_id = module.app_alb_sg.id # to which security_group we are assigning the rule
+}
+
+resource "aws_security_group_rule" "backend_vpn" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.id 
+  security_group_id = module.backend_sg.id # to which security_group we are assigning the rule
+}
+
+resource "aws_security_group_rule" "backend_vpn_8080" {
+  type              = "ingress" # we are passing ingress rules 
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.id 
+  security_group_id = module.backend_sg.id # to which security_group we are assigning the rule
 }
